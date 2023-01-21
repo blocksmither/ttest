70e55f5fd9b94d24c505d8c6e27f5a84b143d930
blocksmither <10137074+blocksmither@users.noreply.github.com>
2023-01-22 04:48:36 +0900

    Watch multiple routers for swap greater than threshold

--- mempool.py
+++ mempool.py
@@ -2,14 +2,15 @@ import argparse
 import datetime
 import json
 import os
+from web3 import Web3
 from decimal import Decimal
 
 import brownie
 import websocket
 import yaml
 from connectors import connectors
-
 from comparator import compare
+from swap import parse_swap_tx_blocknative, get_v2_pair, get_v2_pair_reserves
 
 
 def symbol_dec(symbol):
@@ -24,15 +25,21 @@ class MempoolReader():
     with open(os.path.join(os.path.dirname(__file__), 'config.yaml')) as file:
         config = yaml.safe_load(file)
 
-    def __init__(self, pair, swap, network):
+    def __init__(self, pair, swap, network, threshold):
         self.pair = pair
         self.swap = swap
         self.network = network
         self.connector = eval(f"connectors.{swap}('{network}')")
-        self.address = self.config['networks'][self.network]['pairs'][self.swap][self.pair]
         self.wsapp = websocket.WebSocketApp("wss://api.blocknative.com/v0", on_open=self.on_open, on_message=self.on_message)
+        self.address1 = self.config['networks'][self.network]['exchangeRouters']['UniswapV2']
+        self.address2 = self.config['networks'][self.network]['exchangeRouters']['UniswapV3']
+        self.address3 = self.config['networks'][self.network]['exchangeRouters']['UniswapV302']
+        self.address4 = self.config['networks'][self.network]['exchangeRouters']['Sushiswap']
         self.solBotProject = brownie.project.load('./solidity')
         self.solBotProject.load_config()
+        self.w3 = Web3(Web3.HTTPProvider(
+            self.config['networks'][self.network]['web3Provider']))
+        self.check_threshold = threshold
 
     @property
     def mempool_network(self):
@@ -45,50 +52,42 @@ class MempoolReader():
     def on_message(self, wsapp, message):
         event = json.loads(message)
         try:
-            event['event']['transaction']['hash']
-            print(datetime.datetime.now())
-            print(f"hash: {event['event']['transaction']['hash']}")
-            print(f"block: {event['event']['transaction']['blockNumber']}")
+            swaps = parse_swap_tx_blocknative(event)
+            for swap in swaps:
+                print(swap)
+                if swap.dex_name == 'uniswapv2' or swap.dex_name == 'uniswapv302' or swap.dex_name == 'sushiswap':
+                    pair_address = get_v2_pair(
+                        self.w3, swap.token_in, swap.token_out, swap.dex_name)
+                    reserves = get_v2_pair_reserves(
+                        self.w3, pair_address, swap.dex_name)
+                    if (swap.token_in_amount / reserves['token0'] > self.check_threshold
+                            or swap.token_out_amount / reserves['token1'] > self.check_threshold):
+                        print(
+                            "Possible Arbitrage opportunity swap amount is greater than treshold ", self.check_treshold)
+                        try:
+                            brownie.network.connect(network='mainnet-fork', launch_rpc=True)
+                            solidityBot = self.solBotProject.Bot.deploy({'from': brownie.accounts[0]})
+                            solidityBot.depositETH({'from': brownie.accounts[0], 'value': 10e18})
+                            balances = solidityBot.getBalances()
+                            print(balances)
+                            # Call solidityBot.multiswap({'from': accounts[0])
+                            swap_args = compare(network=self.network, return_swap_args=True)
+                            solidityBot.multiSwap(
+                                swap_args['WETH-USDC']['inToken'],
+                                swap_args['WETH-USDC']['arbToken'],
+                                1e18,
+                                swap_args['WETH-USDC']['dexs'],
+                                False,
+                                {'from': brownie.accounts[0]}
+                            )
+                            balances = solidityBot.getBalances()
+                            print(balances)
+                            brownie.network.disconnect()
+                        except Exception as e:
+                            print('Failed to test bot on ganache fork')
+                            print(e)
 
-            op_type = 1
-
-            for nbc in event['event']['transaction']['netBalanceChanges']:
-                if nbc['address'].lower() == self.address.lower():
-                    for bc in nbc['balanceChanges']:
-                        print(f"delta: {Decimal(bc['delta']) / 10 ** symbol_dec(bc['asset']['symbol'])}, asset: {bc['asset']['symbol']}")
-                        op_type *= float(bc['delta'])
-            if op_type > 0:
-                print("ADDING/RETRIEVING LIQUIDITY")
-            else:
-                print("SWAPING")
-                if event['event']['transaction']['blockNumber'] is None:
-                    self.connector.predict_price(self.pair, event['event']['transaction']['netBalanceChanges'])
-                    # Test Code that Spin up a fork, broadcast the transaction, then try to swap against it.
-                    try:
-                        brownie.network.connect(network='mainnet-fork', launch_rpc=True)
-                        solidityBot = self.solBotProject.Bot.deploy({'from': brownie.accounts[0]})
-                        solidityBot.depositETH({'from': brownie.accounts[0], 'value': 10e18})
-                        balances = solidityBot.getBalances()
-                        print(balances)
-                        # Call solidityBot.multiswap({'from': accounts[0])
-                        swap_args = compare(network=self.network, return_swap_args=True)
-                        solidityBot.multiSwap(
-                            swap_args['WETH-USDC']['inToken'],
-                            swap_args['WETH-USDC']['arbToken'],
-                            1e18,
-                            swap_args['WETH-USDC']['dexs'],
-                            False,
-                            {'from': brownie.accounts[0]}
-                        )
-                        balances = solidityBot.getBalances()
-                        print(balances)
-                        brownie.network.disconnect()
-                    except Exception as e:
-                        print('Failed to test bot on ganache fork')
-                        print(e)
-                else:
-                    print("Too slow. Can't predict price. Tx is already in the blockchain and price changed.")
-        except:
+        except Exception as e:
             pass
 
     def on_open(self, wsapp):
@@ -109,7 +108,7 @@ class MempoolReader():
             "categoryCode": "accountAddress",
             "eventCode": "watch",
             "account": {
-                "address": self.address
+                "address": self.address1,
             },
             "timeStamp": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f%Z"),
             "dappId": "cf6aaedd-10c1-45bd-8970-be4ee9544d9e",
@@ -121,12 +120,60 @@ class MempoolReader():
         }
         wsapp.send(json.dumps(data))
 
+        data = {
+            "categoryCode": "accountAddress",
+            "eventCode": "watch",
+            "account": {
+                "address": self.address2,
+            },
+            "timeStamp": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f%Z"),
+            "dappId": "cf6aaedd-10c1-45bd-8970-be4ee9544d9e",
+            "version": "1",
+            "blockchain": {
+                "system": "ethereum",
+                "network": self.mempool_network
+            }
+        }
+        wsapp.send(json.dumps(data))
+
+        data = {
+            "categoryCode": "accountAddress",
+            "eventCode": "watch",
+            "account": {
+                "address": self.address3,
+            },
+            "timeStamp": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f%Z"),
+            "dappId": "cf6aaedd-10c1-45bd-8970-be4ee9544d9e",
+            "version": "1",
+            "blockchain": {
+                "system": "ethereum",
+                "network": self.mempool_network
+            }
+        }
+        wsapp.send(json.dumps(data))
+
+        data = {
+            "categoryCode": "accountAddress",
+            "eventCode": "watch",
+            "account": {
+                "address": self.address4,
+            },
+            "timeStamp": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f%Z"),
+            "dappId": "cf6aaedd-10c1-45bd-8970-be4ee9544d9e",
+            "version": "1",
+            "blockchain": {
+                "system": "ethereum",
+                "network": self.mempool_network
+            }
+        }
+        wsapp.send(json.dumps(data))
     def start(self):
         print("Reading mempool of:")
-        print(f"Pair: {self.pair}")
-        print(f"Swap: {self.swap}")
         print(f"Network: {self.network}")
-        print(f"Address: {self.address}")
+        print(f"Address: {self.address1}")
+        print(f"Address: {self.address2}")
+        print(f"Address: {self.address3}")
+        print(f"Address: {self.address4}")
         self.wsapp.run_forever()
 
 
@@ -134,12 +181,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="""
     This script prints transactions in the mempool
     """)
-    parser.add_argument("-s", '--swap', default='UniswapV3', choices=["Sushiswap", "UniswapV2", "UniswapV3"], help="Swap")
-    parser.add_argument("-n", "--network", default='mainnet', choices=['mainnet', 'goerli'], help="Select mainnet or testnet network")
+    parser.add_argument("-s", '--swap', default='UniswapV3',
+                        choices=["Sushiswap", "UniswapV2", "UniswapV3"], help="Swap")
+    parser.add_argument("-n", "--network", default='mainnet',
+                        choices=['mainnet', 'goerli'], help="Select mainnet or testnet network")
+    parser.add_argument("-t", "--threshold", default=0.01,
+                        help="Threshold amount for possible arbitrage, should typically be 0.01 or greater")
 
     args = parser.parse_args()
     SWAP = args.swap
     NETWORK = args.network.strip()
+    THRESHOLD = float(args.threshold)
 
-    reader = MempoolReader('USDC-WETH', SWAP, NETWORK)
+    reader = MempoolReader('USDC-WETH', SWAP, NETWORK, THRESHOLD)
     reader.start()
--- swap.py
+++ swap.py
@@ -5,6 +5,7 @@ import yaml
 with open('./config.yaml', 'r') as f:
     config = yaml.safe_load(f)
 
+
 @dataclass
 class RouterSwap:
     token_in: str
@@ -12,17 +13,25 @@ class RouterSwap:
     token_out: str
     token_out_amount: int
     router_address: str
+    swap_method: str
     dex_name: str
 
+
 def parse_swap_tx_blocknative(blocknative_data):
-    subcalls = blocknative_data['event']['contractCall']['subCalls']
-    to_address = blocknative_data['event']['to']
+    if 'subCalls' in blocknative_data['event']['contractCall']:
+        subcalls = blocknative_data['event']['contractCall']['subCalls']
+    else:
+        # cannot parse swap in current logic so stop
+        raise Exception('Cannot parse swap data that does not list subCalls')
+
+    to_address = blocknative_data['event']['transaction']['to']
     swaps = []
     for subcall in subcalls:
         try:
             swaps.append(get_swap_blocknative(subcall, to_address))
         except Exception as e:
             print('!! Error getting swap : ', e)
+    return swaps
 
 
 # TODO  use estimate inout methods instead of minmax for some functions
@@ -31,15 +40,16 @@ def get_swap_blocknative(subcall, router_address):
     params = subcall['data']['params']
     config_router = config['networks']['mainnet']['exchangeRouters']
     if router_address == config_router['UniswapV3']:
-       dex_name = 'uniswapv3'
+        dex_name = 'uniswapv3'
     elif router_address == config_router['UniswapV302']:
-       dex_name = 'uniswapv302'
+        dex_name = 'uniswapv302'
     elif router_address == config_router['UniswapV2']:
-       dex_name = 'uniswapv2'
+        dex_name = 'uniswapv2'
     elif router_address == config_router['Sushiswap']:
-       dex_name = 'sushiswap'
+        dex_name = 'sushiswap'
     else:
-        raise Exception("Swap Router is not recognized in to address of transaction: %s" % (router_address))
+        raise Exception(
+            "Swap Router is not recognized in to address of transaction: %s" % (router_address))
 
     match call_method:
         # Uniswap V3 Router Methods
@@ -47,13 +57,14 @@ def get_swap_blocknative(subcall, router_address):
             # params key nested twice
             params = params['params']
             return RouterSwap(
-                    token_in = params['tokenIn'],
-                    token_in_amount = int(params['amountIn']),
-                    token_out = params['tokenOut'],
-                    token_out_amount = int(params['amountOutMinimum']),
-                    dex_name = dex_name,
-                    router_address = router_address
-                    )
+                token_in=params['tokenIn'],
+                token_in_amount=int(params['amountIn']),
+                token_out=params['tokenOut'],
+                token_out_amount=int(params['amountOutMinimum']),
+                dex_name=dex_name,
+                swap_method='exactInputSingle',
+                router_address=router_address
+            )
         case 'exactInput':
             # This has multiple pools
             raise Exception("No handle for swap method %s" % call_method)
@@ -61,13 +72,14 @@ def get_swap_blocknative(subcall, router_address):
             # params key nested twice
             params = params['params']
             return RouterSwap(
-                    token_in = params['tokenIn'],
-                    token_in_amount = int(params['amountInMax']),
-                    token_out = params['tokenOut'],
-                    token_out_amount = int(params['amountOut']),
-                    dex_name = dex_name,
-                    router_address = router_address
-                    )
+                token_in=params['tokenIn'],
+                token_in_amount=int(params['amountInMaximum']),
+                token_out=params['tokenOut'],
+                token_out_amount=int(params['amountOut']),
+                dex_name=dex_name,
+                swap_method='exactOutputSingle',
+                router_address=router_address
+            )
         case 'exactOutput':
             # This has multiple pools
             raise Exception("No handle for swap method %s" % call_method)
@@ -82,13 +94,14 @@ def get_swap_blocknative(subcall, router_address):
             if len(params['path']) > 2:
                 raise Exception('Cannot parse v2 swap path across >1 pairs')
             return RouterSwap(
-                    token_in = params['path'][0],
-                    token_in_amount = int(params['amountIn']),
-                    token_out = params['path'][1],
-                    token_out_amount = int(params['amountOutMin']),
-                    dex_name = dex_name,
-                    router_address = router_address
-                    )
+                token_in=params['path'][0],
+                token_in_amount=int(params['amountIn']),
+                token_out=params['path'][1],
+                token_out_amount=int(params['amountOutMin']),
+                dex_name=dex_name,
+                swap_method='swapExactTokensForTokens',
+                router_address=router_address
+            )
         case 'swapTokensforExactTokens':
             # This can affect multiple pairs if path length > 2
             # Example if len(path)==3 then 2 pairs are affected
@@ -99,13 +112,14 @@ def get_swap_blocknative(subcall, router_address):
             if len(params['path']) > 2:
                 raise Exception('Cannot parse v2 swap path across >1 pairs')
             return RouterSwap(
-                    token_in = params['path'][0],
-                    token_in_amount = int(params['amountOut']),
-                    token_out = params['path'][1],
-                    token_out_amount = int(params['amountInMax']),
-                    dex_name = dex_name,
-                    router_address = router_address
-                    )
+                token_in=params['path'][0],
+                token_in_amount=int(params['amountOut']),
+                token_out=params['path'][1],
+                token_out_amount=int(params['amountInMax']),
+                dex_name=dex_name,
+                swap_method='swapTokensForExactTokens',
+                router_address=router_address
+            )
         case 'swapExactETHForTokens':
             raise Exception("No handle for swap method %s" % call_method)
         case 'swapTokensForExactEth':
@@ -120,3 +134,41 @@ def get_swap_blocknative(subcall, router_address):
             raise Exception("No handle for swap method %s" % call_method)
         case 'swapExactTokensForETHSupportingFeeOnTransferTokens':
             raise Exception("No handle for swap method %s" % call_method)
+        case _:
+            raise Exception(
+                "Unrecognized contractcall method %s" % call_method)
+
+
+def get_v2_pair(w3, token_in, token_out, dex_name):
+    if dex_name == 'uniswapv2' or dex_name == 'uniswapv302':
+        with open('./interfaces/uniswapv2/factory.abi', 'r') as f:
+            factory_abi = f.read().rstrip()
+        factory_address = config['networks']['mainnet']['exchangeFactories']['UniswapV2']
+    elif dex_name == 'sushiswap':
+        with open('./interfaces/sushiswap/factory.abi', 'r') as f:
+            factory_abi = f.read().rstrip()
+        factory_address = config['networks']['mainnet']['exchangeFactories']['Sushiswap']
+    else:
+        raise Exception("Cannot run get_v2_pair on dex %s" % dex_name)
+
+    factory_contract = w3.eth.contract(
+        address=factory_address, abi=factory_abi)
+    pair_address = factory_contract.functions.getPair(
+        token_in, token_out).call()
+    return pair_address
+
+
+def get_v2_pair_reserves(w3, pair_address, dex_name):
+    if dex_name == 'uniswapv2' or dex_name == 'uniswapv302':
+        with open('./interfaces/uniswapv2/pair.abi', 'r') as f:
+            pair_abi = f.read().rstrip()
+    elif dex_name == 'sushiswap':
+        with open('./interfaces/sushiswap/pair.abi', 'r') as f:
+            pair_abi = f.read().rstrip()
+    else:
+        raise Exception("Cannot run get_v2_pair_reserves on dex %s" % dex_name)
+
+    pair_contract = w3.eth.contract(address=pair_address, abi=pair_abi)
+    (token0_reserve, token1_reserve,
+     last_block_timestamp) = pair_contract.functions.getReserves().call()
+    return {"token0": int(token0_reserve), "token1": int(token1_reserve)}
